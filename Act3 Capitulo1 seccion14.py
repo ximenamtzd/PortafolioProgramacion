0a212293af1580523e81f7b0c027a7b901f88083
ximenamtzd <ximenita.mtz.diaz@gmail.com>
2025-01-25 21:53:43 -0600

    termine act3

--- Act3 Capitulo1 seccion14.py
+++ Act3 Capitulo1 seccion14.py
@@ -1,8 +1,33 @@
 print('respuesta1')
 print('a')
 
+print("respuesta 2")
+
+print("son secuencias o conjuntos o intructiones de un lenguaje de programacion para que las ejecute la computadora")
+
+print("un compilador traduce el codigo fuente de un programa a un codigo maquina antes de ejecutarlo y el interprete traduce el codigo linea a linea a medida que se ejecuta")
+
+print("respuesta4")
+
+print("el codigo fuente de Python")
+
+print("respuesta 5")
+
+print("lo que esta mal en el codigo es que en la imprecion faltan los parentesis ya que sin esos no se imprime la palabra y aparte la palabra reservada esta mal escrita ")
+
+print("respuesta 6")
+
+print("memoria principal")
+
+print("respuesta 7")
+
+print("44")
+
+print("respuesta 8")
+
+print("1- el cerebro humano, 2- memoria a corto plazo, 3- memoria a largo plazo, 4- los sentidos, 5- la comunication")
+
+print("respuesta 9")
+
+print("primero verificar que parte del codigo esta causando problema, despues leer el mensaje de error, revisar el codigo, verificar la estructura del codigo, prueba por partes, y volver a hacer correr el codigo")
 print("Hola mundo")
-
-edad = int(input("Cual es su edad?: "))
-
-print("Su edad es: %d" % edad)
