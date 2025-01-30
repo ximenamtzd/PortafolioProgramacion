f31f20a4c083796c8de279f1c6fabd03222e7ab8
ximenamtzd <ximenita.mtz.diaz@gmail.com>
2025-01-30 10:12:20 -0600

    sesion 8

---
+++ m2.py
@@ -0,0 +1,55 @@
+#Aritmeticos
+#PEMDAS
+
+#P()   (var)
+#E ^2   var^90
+#M *    a*b
+#D /    a/b
+#A +    a+b
+#S -    a-b
+
+#Operadores contraccion
+'''''
+prod *= prod^2
+sum += 1
+res -= 1
+mult *= 2
+div /=2
+
+
+a // b  10//3 = 3, 3/3= 3/3 + 4/3 = 10/3 3 residuio, numerador = 3
+a % b   10%10 = 1
+
+
+#Logicos
+obj1 and obj2 1 & 4
+
+obj3 or obj4  3 or 5
+
+not object = None 
+
+''''''
+
+#Relacionales
+
+
+< a < b True | False
+> a > True | False
+<= a <= b 
+>= a >= b
+== a==b
+!= a != 0 
+
+
+'''
+
+
+#def nombre de la funcion y sus parametros
+
+def suma(a,b):
+    return(a + b)
+
+def resta(a,b):
+    return(a - b)    
+
+print (suma(10,15) , resta(10,15))
